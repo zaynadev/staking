7a6cdf155f5f3725ca28ceab828d465c8ec827a1
Zainab Oufqir <zayna.oufkir@gmail.com>
2023-02-23 20:43:10 +0100

    add deploy script for staking contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,11 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
-async function main() {}
+async function main() {
+  const Staking = await ethers.getContractFactory("Staking");
+  const staking = await Staking.deploy({ value: ethers.utils.parseEther("10") });
+  await staking.deployed();
+  console.log(`contract deployed at: ${staking.address}`);
+}
 
 main().catch((error) => {
   console.error(error);
