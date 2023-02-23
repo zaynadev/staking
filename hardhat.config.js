dfcee8b833680250f7ed4fd421dc0ec5e06a78e8
Zainab Oufqir <zayna.oufkir@gmail.com>
2023-02-23 15:41:12 +0100

    init project

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
+};
